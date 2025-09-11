b001156c9e707b65083a914816773e5d85c4f16b
ParkSungGyu1 <steven0507@naver.com>
2025-09-11 10:07:35 +0900

    lv4

--- src/main/java/com/example/sparta/repository/ProductRepository.java
+++ src/main/java/com/example/sparta/repository/ProductRepository.java
@@ -3,5 +3,8 @@ package com.example.sparta.repository;
 import com.example.sparta.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByIdIn(List<Long> productIds);
 }
--- src/main/java/com/example/sparta/service/OrderService.java
+++ src/main/java/com/example/sparta/service/OrderService.java
@@ -8,6 +8,7 @@ import com.example.sparta.repository.OrderLineRepository;
 import com.example.sparta.repository.OrderRepository;
 import com.example.sparta.repository.ProductRepository;
 import com.example.sparta.dto.OrderCreateRequest;
+import com.example.sparta.util.OrderServiceSupport;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,16 +21,25 @@ import java.util.List;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final OrderLineService orderLineService;
+    private final OrderLineRepository orderLineRepository;
+
+    private final ProductRepository productRepository;
+
 
     @Transactional
     public Order create(OrderCreateRequest request) {
-        // 주문 데이터 생성
+        // 주문 생성
         Order order = orderRepository.save(new Order(request.getTotalPrice()));
 
-        // 주문 상세 데이터 생성
-        orderLineService.createOrderLines(order, request.getOrderLines());
+        // 주문 요청한 상품의 ID 리스트
+        List<Long> productIds = request.getOrderLines().stream()
+                .map(OrderLineRequest::getProductId)
+                .toList();
 
+        // 주문 요청한 상품 리스트 조회
+        List<Product> products = productRepository.findByIdIn(productIds);
+        List<OrderLine> orderLineList = OrderServiceSupport.buildOrderLines(products, request.getOrderLines(), order);
+        orderLineRepository.saveAll(orderLineList);
         return order;
     }
 }
